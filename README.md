82d3133d72af969fe86eed3eb7de8d971ab5e0d4
Judit Ross <jtrs@incowia.com>
2017-12-14 12:53:10 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-Uploader for manifest.webpackeges in store (odin-for-asgard)
+# Cubbles testdata uploader 
+
+Generate a webpackage and upload this the base. The ganaration based on one or more valid manifest files.   
+* Generate the directory structur for upload, 
+* generate missed necessary files like sorce files for elementary components
+* complete manifest with the reference of generated files  
+* and upload this to the defined store url.
+
+The source manifest files must contain a valid webpackage manifest.
+ 
+## Install
+  npm install cubx-testdata-uploader
+  
+## Usage: 
+  cubx-testdata-uploader -u \<url\> -s \<source path\> \[-p \<proxy\>\]
+    
+Use the The following options:     
+* -u,--storeUrl - The url of the store like https://cubbles.world/odin-test 
+* -s,--sourcePath - The source path. Place the manifest files as *.json files in this folder.
+* -p,--proxy - the proxy url, if use behind a proxy   
--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 var fs = require('fs');
 var path = require('path');
 var FileGenerator = require('./lib/file-generator.js');
-var CouchUploader = require('./lib/couch-uploader.js');
+// var CouchUploader = require('./lib/couch-uploader.js');
+var Uploader = require('cubx-webpackage-uploader');
 
 var argv = require('yargs')
-  .usage('Usage: Usage: $0 -u,--storeUrl [storeUrl] -s,--sourcePath [sourcePath]')
+  .usage('Usage: $0 -u,--storeUrl <storeUrl> -s,--sourcePath <sourcePath> [-p, --proxy <proxyUrl>]')
   .demandOption([ 'u', 's' ])
   .alias('u', 'storeUrl')
   .alias('s', 'sourcePath')
+  .alias('p', 'proxy')
   .locale('en')
   .argv;
 
 var storeUrl = argv.storeUrl;
+var proxy = argv.proxy;
 // TODO check storeUrl
-var regex = /^https?:\/\/[a-z-.]*:\d{4}\/webpackage-store-[a-z0-9-]*$/gi;
+var regex = /^https?:\/\/[a-z-.]*(:\d{1,4})?\/[a-z0-9-]*$/i;
 if (!storeUrl.match(regex)) {
   console.error('Not a valid store url. The store url must match ' + regex);
 }
 var sp = storeUrl.split('/');
-var basicUrl = sp[ 0 ] + '//' + sp[ 2 ];
+// var basicUrl = sp[ 0 ] + '//' + sp[ 2 ];
 var store = sp[ 3 ];
 
 var source = argv.sourcePath;
@@ -28,14 +31,36 @@ fs.stat(sourcePath, (error, stats) => {
   if (error) {
     console.error(error, sourcePath);
   } else if (stats.isDirectory()) {
-    var couchUploader = new CouchUploader(basicUrl, couchDocsPath);
+    // var couchUploader = new CouchUploader(basicUrl, couchDocsPath);
+    var uploader = new Uploader();
     var fileGenerator = new FileGenerator(sourcePath, couchDocsPath, store);
+    var uploadConfig = {
+      source: '',
+      target: {
+        url: storeUrl,
+        path: '_api/upload',
+        proxy: ''
+      }
+    };
+    // If the argument -p or --proxy exists use proxy
+    if (proxy && proxy.length > 0) {
+      uploadConfig.target.proxy = proxy;
+    }
     var promise = fileGenerator.createUploadStructure();
     promise.then(
-      (value) => {
-        couchUploader.upload();
-      },
-      (err) => {
+      (values) => {
+        // upload all webpacakges
+        values.forEach(path => {
+          uploadConfig.source = path;
+          uploader.uploadSingleWebpackage(uploadConfig, function (err, success) {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('Uploaded successed webpackage from ' + path);
+            }
+          });
+        });
+      }).catch(err => {
         console.error(err);
       });
   } else {
